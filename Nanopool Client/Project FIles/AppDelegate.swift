a452ff800e9e6d190da28a08e481832e0f764df8
Florin Uscatu <florinusc@gmail.com>
2019-07-11 09:03:35 +0300

    Adds basic folder structure

---
+++ Nanopool Client/Project FIles/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  Nanopool Client
+//
+//  Created by Florin Uscatu on 11/07/2019.
+//  Copyright © 2019 Florin Uscatu. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
