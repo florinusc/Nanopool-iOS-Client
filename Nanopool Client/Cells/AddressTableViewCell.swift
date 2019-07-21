6978c6380f37b0cce24fd852d7d9d2e1f104d31f
Florin Uscatu <florinusc@gmail.com>
2019-07-21 17:45:56 +0300

    Add table view and dummy cells to address list view controller

---
+++ Nanopool Client/Cells/AddressTableViewCell.swift
@@ -0,0 +1,11 @@
+//
+//  AddressTableViewCell.swift
+//  Nanopool Client
+//
+//  Created by Florin Uscatu on 21/07/2019.
+//  Copyright © 2019 Florin Uscatu. All rights reserved.
+//
+
+import UIKit
+
+class AddressTableViewCell: UITableViewCell {}
--- Nanopool Client/ViewControllers/AddressListViewController.swift
+++ Nanopool Client/ViewControllers/AddressListViewController.swift
@@ -8,7 +8,37 @@
 
 import UIKit
 
-class AddressListViewController: UIViewController {}
+class AddressListViewController: UIViewController {
+    
+    // MARK: - IBOutlets
+    @IBOutlet private weak var tableView: UITableView!
+    
+    // MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setup()
+    }
+    
+    // MARK: - Setup
+    private func setup() {
+        setupTableView()
+    }
+    
+    private func setupTableView() {
+        tableView.register(AddressTableViewCell.self)
+    }
+}
+
+extension AddressListViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: AddressTableViewCell = tableView.dequeueReusableCell(for: indexPath)
+        return cell
+    }
+}
 
 extension AddressListViewController: Storyboarded {
     static var storyboardName: Storyboards { return .address }
